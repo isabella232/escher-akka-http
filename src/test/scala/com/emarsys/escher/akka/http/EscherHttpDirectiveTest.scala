161364f9f7291c9a988f2efb09a16cfae413e8f5
Miklos Martin <miklos.martin@gmail.com>
2017-12-04 15:30:08 +0100

    add tests for escherAuthenticate directive

--- src/test/scala/com/emarsys/escher/akka/http/EscherHttpDirectiveTest.scala
+++ src/test/scala/com/emarsys/escher/akka/http/EscherHttpDirectiveTest.scala
@@ -1,14 +1,13 @@
 package com.emarsys.escher.akka.http
 
-import akka.http.scaladsl.model.{HttpMethods, Uri}
 import akka.http.scaladsl.model.headers.RawHeader
-import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.{HttpMethods, HttpRequest, Uri}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.emarsys.escher.akka.http.config.EscherConfig
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.{path => _, _}
+import org.scalatest.{Matchers, WordSpec}
 import spray.json.DefaultJsonProtocol
 
 
@@ -33,7 +32,12 @@ class EscherHttpDirectiveTest
     headers = List(RawHeader("host", "trunk.suite.ett.local"), RawHeader("X-Forwarded-Proto", "http"))
   )
 
-  def route: Route = (get & pathSingleSlash)(checkForwardedHttps(complete("OK")))
+  implicit val logger = system.log
+
+  def route: Route =
+    (get & pathSingleSlash)(checkForwardedHttps(complete("OK"))) ~
+    (get & path("path"))(escherAuthenticate(escherConfig.services)(_ => complete("OK")))
+
   override val escherConfig: EscherConfig =
     new EscherConfig(com.typesafe.config.ConfigFactory.load().getConfig("escher"))
 
@@ -59,5 +63,27 @@ class EscherHttpDirectiveTest
     }
   }
 
+  "escherAuthenticate" should {
+    val service = escherConfig.services.head
+    val (key, secret) = (escherConfig.key(service), escherConfig.secret(service))
+    val host = s"${escherConfig.hostName}:${escherConfig.port}"
 
+    "accept signed requests" in {
+      val signed = signRequest(service)(executor, materializer)(_:HttpRequest).futureValue
+      signed(Get(s"https://$host/path?query=param").withHeaders(List(RawHeader("host", host)))) ~> route ~> check {
+        handled shouldBe true
+        responseAs[String] shouldEqual "OK"
+      }
+    }
+
+    "accept requests with presigned urls" in {
+      val escher = setupEscher(createEscherForSigning(service))
+      val presignedUrl = escher.presignUrl(s"https://$host/path?query=param", key, secret)
+
+      Get(presignedUrl).withHeaders(List(RawHeader("host", host))) ~> route ~> check {
+        handled shouldBe true
+        responseAs[String] shouldEqual "OK"
+      }
+    }
+  }
 }
